571c96ee82b791ec4f149924a1f096d197002474
ILA <ila.comp@gmail.com>
2016-04-21 23:27:30 +0300

    Skeleton

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 app.use(function(req, res) {
-  res.sendfile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // error handlers
